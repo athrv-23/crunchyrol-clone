f2bef7cb0a2076a8a1a917a1dd16661afd7d4377
unknown <workforatharva2304@gmail.com>
2023-11-09 15:21:59 +0530

    javascript added need to add login page

---
+++ javascript/index.js
@@ -0,0 +1,13 @@
+function login() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    if(email == "pravinshinde99300@gmail.com" && password == "pravin123")
+    {
+        alert("access granted")
+    }
+    else
+    {
+        alert("please check your email and password")
+    }
+}
